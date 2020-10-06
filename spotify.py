8dd921a98aad88d42e992bda36484b3f8571fa5d
Minka Tahvanainen <minka.tahvanainen@gmail.com>
2020-10-06 19:19:32 +0300

    Add popularity value from spotify to train data

--- spotify.py
+++ spotify.py
@@ -4,8 +4,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from Utils.utils import client_id, client_secret
 
-colnames=['mxm_track_id', 'track_name', 'artist_id', 'artist_name', 'genre', 'rating', 'num_favourite'] 
-df = pd.read_csv('Datasets/train2.0.csv', names=colnames, header=None)
+df = pd.read_csv('Datasets/train2.0_english.csv')
 
 client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
@@ -13,24 +12,20 @@ sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 #Add new column to store popularity score from spotify
 df["popularity"] = np.nan
 
+#Get popularity values for tracks from spotify
 for index, row in df.iterrows():
+    if(index % 1000 == 0):
+        print(index, " / ", df.shape[0])
     if pd.notnull(row["track_name"]) and pd.notnull(row["artist_name"]):
-        results = sp.search(q='track:' + row["track_name"] +' artist:' + row["artist_name"], type='track', limit=1)
-        if results.get("tracks").get("items"):
-            df["popularity"][index] = results.get("tracks").get("items")[0].get("popularity")
+        try:
+            results = sp.search(q='track:' + row["track_name"] +' artist:' + row["artist_name"], type='track', limit=1)
+            if results.get("tracks").get("items"):
+                df.loc[index, 'popularity'] = results.get("tracks").get("items")[0].get("popularity")
+        except:
+            print("Something went wrong with track " + row["track_name"] + " : " + row["artist_name"])
 
-print("-----Done-----")
+df = df[df['popularity'].notna()]
+print(df.head())
+print(df.notna().sum())
+print(df.info())
 df.to_csv('Datasets/train_popularity.csv', mode='a', header=False, index=False)
-
-
-
-#token_url = "https://accounts.spotify.com/api/token"
-#method = "POST"
-#token_data = {
-#    "grant_type": "client_crendentials"
-#}
-
-#        track_url = urllib.parse.quote(row["track_name"])
-#        artist_url = urllib.parse.quote(row["artist_name"])
-#        url = base_url + "?q=track%3A" + track_url + "%20artist%3" + artist_url + "&type=track&limit=1"
-        
